2b2f6c8717cd970731497d84f2a742be469bc5ab
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-05-23 08:57:09 +0200

    Update Grafica.py
    nuovi

--- Grafica.py
+++ Grafica.py
@@ -17,71 +17,78 @@ root.title("Collegamento con database") #titolo della finestra
 
 
 
-# Creazione delle label
-titolo = Label(root, text="Titolo:") #creazione label "Titolo"
-titolo.grid(row=0, column=0, padx=10, pady=10) #posizione della label
+def artisti_new_tab():
+    root = Tk()
+    root.geometry("400x300")
+    root.title("Collegamento con database in artisti")
+    titolo_artisti= Label(root,text="Artisti:")
+    titolo_artisti.grid(row=0,column=0,padx=10,pady=10) 
+    
+    def mostra():
+        #conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+        #cursor = conn.cursor()
+        #cursor.execute("Select ")
+        #conn.commit()
+        #cursor.close()
+        #conn.close()
+        meringa= Label(root,text="funzia")  
+        meringa.grid(row=4,column=5,padx=10,pady=10)
 
-artista = Label(root, text="Artista:") #creazione label "Artista"
-artista.grid(row=1, column=0, padx=10, pady=10) #posizione della label
 
-anno = Label(root, text="Anno:") #creazione label "Anno"
-anno.grid(row=2, column=0, padx=10, pady=10) #posizione della label
+    buttonMostra = Button(root, text="Mostra", command=mostra)
+    buttonMostra.grid(row=1, column=3, columnspan=2, padx=10, pady=10)
 
-genere = Label(root, text="Genere:") #creazione label "Genere"
-genere.grid(row=3, column=0, padx=10, pady=10) #posizione della label
+buttonArtisti = Button(root, text="Artisti", command=artisti_new_tab)
+buttonArtisti.grid(row=1, column=3, columnspan=2, padx=10, pady=10)
 
-# Creazione dei campi di input
-entry_titolo = Entry(root, width=30) #
-entry_titolo.grid(row=0, column=1, padx=10, pady=10)
+def generi_new_tab():
+     root = Tk()
+     root.geometry("400x300")
+     root.title("Collegamento con database in generi")
+     titolo_generi= Label(root,text="Generi:")  
+     titolo_generi.grid(row=0,column=0,padx=10,pady=10)
 
-entry_artista = Entry(root, width=30)
-entry_artista.grid(row=1, column=1, padx=10, pady=10)
 
-entry_anno = Entry(root, width=30)
-entry_anno.grid(row=2, column=1, padx=10, pady=10)
 
-entry_genere = Entry(root, width=30, )
-entry_genere.grid(row=3, column=1, padx=10, pady=10)
+buttonGeneri = Button(root, text="Generi", command=generi_new_tab)
+buttonGeneri.grid(row=3, column=3, columnspan=2, padx=10, pady=10)
+
+
+def canzoni_new_tab():
+    root = Tk()
+    root.geometry("400x300")
+    root.title("Collegamento con database in generi")
+    titolo_canzoni= Label(root,text="Canzoni:")  
+    titolo_canzoni.grid(row=0,column=0,padx=10,pady=10)
+
+buttonCanzoni=Button(root, text="Canzoni", command=canzoni_new_tab)
+buttonCanzoni.grid(row=4, column=3, columnspan=2, padx=10, pady=10)
+
 
 # Definizione della funzione per l'inserimento dei dati nel database
-def insert_into_database():
+#def insert_into_database():
     # Connessione al database SQL Server
     
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    #conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
     # Recupero dei dati dai campi di input
-    titolo = entry_titolo.get()
-    artista = entry_artista.get()
-    anno = entry_anno.get()
-    genere = entry_genere.get()
+    #titolo = entry_titolo.get()
+    #artista = entry_artista.get()
+    #anno = entry_anno.get()
+    #genere = entry_genere.get()
 
-    # Controllo se tutti i campi sono stati riempiti
-    #if titolo and artista and anno and genere:
-    # Esecuzione della query SQL per l'inserimento dei dati nella tabella
-       # cursor.execute("INSERT INTO Musica (titolo, artista, anno, genere) VALUES (?, ?, ?, ?)", (titolo, artista, anno, genere))
-       # conn.commit()
-    #else:
-     #   print("Errore: tutti i campi devono essere riempiti")
- 
 
     # Esecuzione della query SQL per l'inserimento dei dati nella tabella
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO Musica (titolo, artista, anno, genere) VALUES (?, ?, ?, ?)", (titolo, artista, anno, genere))
-    conn.commit()
-    cursor.close()
+    #cursor = conn.cursor()
+    #cursor.execute("INSERT INTO Musica (titolo, artista, anno, genere) VALUES (?, ?, ?, ?)", (titolo, artista, anno, genere))
+    #conn.commit()
+    #cursor.close()
 
     # Chiusura della connessione al database
-    conn.close()
+    #conn.close()
 
     # Messaggio di conferma dell'inserimento dei dati
-    messagebox.showinfo("Inserimento dati", "I dati sono stati inseriti con successo nel database")
+    #messagebox.showinfo("Inserimento dati", "I dati sono stati inseriti con successo nel database")
 
-
-
-
-
-# Creazione del pulsante per l'inserimento dei dati nel database
-button = Button(root, text="Inserisci", command=insert_into_database)
-button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
 root.mainloop()
