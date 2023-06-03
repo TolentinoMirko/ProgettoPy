5f4e7f17a5e1ed92d2635ad5a3ebc2faeb1988f1
Mattia-Ottaviano <90761518+Mattia-Ottaviano@users.noreply.github.com>
2023-06-03 22:48:32 +0200

    Update Grafica.py

--- Grafica.py
+++ Grafica.py
@@ -86,7 +86,7 @@ def artisti_new_tab():
     
     def visualizza_artisti():
         root = Tk()
-        root.geometry("400x300")
+        root.geometry("500x400")
         root.title("visualizza tabella")
         
         con1 = conn
@@ -97,21 +97,53 @@ def artisti_new_tab():
         i=0 
         for artisti in cur1: 
             for j in range(len(artisti)):
-                e = Entry( root,width=15, fg='blue') 
-                e.grid(row=i, column=j) 
-                e.insert(END, artisti[j])
-            i=i+1    
+                f = Frame(root, background='black')
+                e = ttk.Label(f, width=15, foreground='blue', text=artisti[j], background='white',) 
+                e.pack(padx=1, pady=1)
+                f.grid(row=i, column=j)
+            buttonupdate = ttk.Button(root, text="Update", width=15, command=lambda a=artisti: update_artisti(a))
+            buttonupdate.grid(row=i, column=len(artisti), columnspan=2, padx=10)
+            i= i + 1
 
         root.mainloop()
 
-        buttonupdate = ttk.Button(root, text="Update", command=update_artisti)
-        buttonupdate.grid(row=8, column=1, columnspan=2, padx=10, pady=10)
+        
 
-        def update_artisti():
-            root = Tk()
-            root.geometry("400x300")
-            root.title("update")
+    def update_artisti(artista):
+        root = Tk()
+        root.geometry("400x300")
+        root.title("update")
+        nome_arte = ttk.Label(root, text='Nome d\'arte')
+        nome_arte.grid(row=0, column=0)
+        e_na = ttk.Entry(root)
+        e_na.insert(END, artista[1])
+        e_na.grid(row=0, column=1)
+        nome = ttk.Label(root, text='Nome')
+        nome.grid(row=1, column=0)
+        e_n = ttk.Entry(root)
+        e_n.insert(END, artista[2])
+        e_n.grid(row=1, column=1)
+        cognome = ttk.Label(root, text='Cognome')
+        cognome.grid(row=2, column=0)
+        e_c = ttk.Entry(root)
+        e_c.insert(END, artista[3])
+        e_c.grid(row=2, column=1)
+        
+        btn = ttk.Button(root, text="Update", command=lambda id=artista[0]: conferma_artisti(id, e_na.get(), e_n.get(), e_c.get()))
+        btn.grid(sticky='nsew', column=0, columnspan=2, row=3)
 
+    def conferma_artisti(id, na, n, c):
+        query = "UPDATE artisti SET nome_d_arte = ?, nome = ?, cognome = ? WHERE id = ?"
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server +
+                              ';DATABASE='+database+';UID='+username+';PWD=' + password)
+
+        # Esecuzione della query SQL per l'inserimento dei dati nella tabella
+        cursor = conn.cursor()
+        cursor.execute(query, (na, n, c, id))
+        conn.commit()
+        cursor.close()
+        messagebox.showinfo(
+            "Inserimento dati", "I dati sono stati inseriti con successo nel database")
     
     titolo_artisti = ttk.Label(
         root, text="Che azione desideri effettuare con gli artisti?")
@@ -182,15 +214,44 @@ def generi_new_tab():
         cur1 = con1.cursor()
 
         cur1.execute("SELECT * FROM genere")
-        i=0 
-        for genere in cur1: 
-            for j in range(len(genere)):
-                e = Entry( root,width=15, fg='blue') 
-                e.grid(row=i, column=j) 
-                e.insert(END, genere[j])
-            i=i+1    
+        i=0
+        for generi in cur1: 
+            for j in range(len(generi)):
+                f = Frame(root, background='black')
+                e = ttk.Label(f, width=15, foreground='blue', text=generi[j], background='white') 
+                e.pack(padx=1, pady=1)
+                f.grid(row=i, column=j)
+            buttonupdate = ttk.Button(root, text="Update", width=15, command=lambda g=generi: update_generi(g))
+            buttonupdate.grid(row=i, column=len(generi), padx=10)
+            i= i + 1
 
         root.mainloop()
+
+
+    def update_generi(generi):
+        root = Tk()
+        root.geometry("400x300")
+        root.title("update")
+        nome = ttk.Label(root, text='Nome')
+        nome.grid(row=0, column=0)
+        e_n = ttk.Entry(root)
+        e_n.insert(END, generi[1])
+        e_n.grid(row=0, column=1)
+    
+        btn = ttk.Button(root, text="Update", command=lambda id=generi[0]: conferma_generi(id, e_n.get()))
+        btn.grid(sticky='nsew', column=0, columnspan=2, row=1)
+
+    def conferma_generi(id, n):
+        query = "UPDATE genere SET nome = ? WHERE id = ?"
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server +
+                            ';DATABASE='+database+';UID='+username+';PWD=' + password)
+        
+        cursor = conn.cursor()
+        cursor.execute(query, (n, id))
+        conn.commit()
+        cursor.close()
+        messagebox.showinfo(
+            "Inserimento dati", "I dati sono stati inseriti con successo nel database")
 
     titolo_generi = ttk.Label(root, text="Che azione desideri effettuare con i generi?")
     titolo_generi.grid(row=0, column=0, padx=10, pady=10)
@@ -277,15 +338,59 @@ def canzoni_new_tab():
         cur1 = con1.cursor()
 
         cur1.execute("SELECT * FROM canzoni")
-        i=0 
-        for canzone in cur1: 
-            for j in range(len(canzone)):
-                e = Entry( root,width=15, fg='blue') 
-                e.grid(row=i, column=j) 
-                e.insert(END, canzone[j])
-            i=i+1    
+        i=0
+        for canzoni in cur1: 
+            for j in range(len(canzoni)):
+                f = Frame(root, background='black')
+                e = ttk.Label(f, width=15, foreground='blue', text=canzoni[j], background='white') 
+                e.pack(padx=1, pady=1)
+                f.grid(row=i, column=j)
+            buttonupdate = ttk.Button(root, text="Update", width=15, command=lambda c=canzoni: update_canzoni(c))
+            buttonupdate.grid(row=i, column=len(canzoni), padx=10)
+            i= i + 1    
 
         root.mainloop()
+
+    def update_canzoni(canzoni):
+        root = Tk()
+        root.geometry("400x300")
+        root.title("update")
+        artista_id = ttk.Label(root, text='artista_id')
+        artista_id.grid(row=0, column=0)
+        e_ida = ttk.Entry(root)
+        e_ida.insert(END, canzoni[1])
+        e_ida.grid(row=0, column=1)
+        genere_id = ttk.Label(root, text='genere_id')
+        genere_id.grid(row=1, column=0)
+        e_idg = ttk.Entry(root)
+        e_idg.insert(END, canzoni[2])
+        e_idg.grid(row=1, column=1)
+        anno = ttk.Label(root, text='anno')
+        anno.grid(row=2, column=0)
+        e_a = ttk.Entry(root)
+        e_a.insert(END, canzoni[3])
+        e_a.grid(row=2, column=1)
+        nome_canzone = ttk.Label(root, text='nome_canzone')
+        nome_canzone.grid(row=3, column=0)
+        e_nc = ttk.Entry(root)
+        e_nc.insert(END, canzoni[4])
+        e_nc.grid(row=3, column=1)
+        
+        btn = ttk.Button(root, text="Update", command=lambda id=canzoni[0]: conferma_canzoni(id, e_ida.get(), e_idg.get(), e_a.get(), e_nc.get()))
+        btn.grid(sticky='nsew', column=0, columnspan=2, row=4)
+
+
+    def conferma_canzoni(id, art_id, gen_id, anno, canz_nome):
+        query = "UPDATE canzoni SET artista_id = ?, genere_id = ?, anno = ?, nome_canzone = ? WHERE id = ?"
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server +
+                            ';DATABASE='+database+';UID='+username+';PWD=' + password)
+        
+        cursor = conn.cursor()
+        cursor.execute(query, (art_id, gen_id, anno, canz_nome, id))
+        conn.commit()
+        cursor.close()
+        messagebox.showinfo(
+            "Inserimento dati", "I dati sono stati inseriti con successo nel database")
 
     titolo_canzoni = ttk.Label(root, text="Che azione desideri effettuare con le canzoni?")
     titolo_canzoni.grid(row=0, column=0, padx=10, pady=10)
